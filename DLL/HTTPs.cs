efc31104cd2cfb163304a0ea86144e52c9e75410
ahmedadelab <117776635+ahmedadelab@users.noreply.github.com>
2023-11-08 16:00:41 +0200

    updated

--- DLL/HTTPs.cs
+++ DLL/HTTPs.cs
@@ -9,7 +9,7 @@ namespace VascoAPI
             var MyConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             var url = MyConfig.GetValue<string>("AppSettings:URL");
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
-            //    webRequest.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            webRequest.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
             webRequest.Headers.Add(@"SOAP:Action");
             webRequest.ContentType = "text/xml;charset=\"utf-8\"";
             webRequest.Accept = "text/xml";

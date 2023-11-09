f5e2437f24a641ef655ce85f8ba55071d31f25b7
ahmedadelab <117776635+ahmedadelab@users.noreply.github.com>
2023-11-09 10:09:21 +0200

    updated

--- DLL/HTTPs.cs
+++ DLL/HTTPs.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
 namespace VascoAPI
@@ -9,10 +10,16 @@ namespace VascoAPI
         {
             var MyConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             var url = MyConfig.GetValue<string>("AppSettings:URL");
-            var Cert1 = MyConfig.GetValue<string>("AppSettings:Cert");
-            X509Certificate Cert = X509Certificate.CreateFromCertFile(Cert1);
+          //  var Cert1 = MyConfig.GetValue<string>("AppSettings:Cert");
+         //   X509Certificate Cert = X509Certificate.CreateFromCertFile(Cert1);
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
-            webRequest.ClientCertificates.Add(Cert);
+            //  webRequest.ClientCertificates.Add(Cert);
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // Pass the handler to httpclient(from you are calling api)
+            HttpClient client = new HttpClient(clientHandler);
+            webRequest.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
             webRequest.Headers.Add(@"SOAP:Action");
             webRequest.ContentType = "text/xml;charset=\"utf-8\"";
             webRequest.Accept = "text/xml";

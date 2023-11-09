26c34d3a01525155cb8dee178e3590a067511836
ahmedadelab <117776635+ahmedadelab@users.noreply.github.com>
2023-11-09 09:42:30 +0200

    cert

--- DLL/HTTPs.cs
+++ DLL/HTTPs.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
 namespace VascoAPI
 {
@@ -8,8 +9,9 @@ namespace VascoAPI
         {
             var MyConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             var url = MyConfig.GetValue<string>("AppSettings:URL");
+            X509Certificate Cert = X509Certificate.CreateFromCertFile("C:\\temp\\PDUATOASIDKV01.eg.albaraka.crt");
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
-            webRequest.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            //webRequest.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
             webRequest.Headers.Add(@"SOAP:Action");
             webRequest.ContentType = "text/xml;charset=\"utf-8\"";
             webRequest.Accept = "text/xml";
